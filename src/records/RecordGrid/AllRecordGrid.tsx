13dc0fff9b365eff90480e527da20b531e16f490
Johannes Ruthmann <j.ruthmann@online.de>
2025-03-15 14:11:11 +0100

    #5: fix pagination, sorting and filtering

--- src/records/RecordGrid/AllRecordGrid.tsx
+++ src/records/RecordGrid/AllRecordGrid.tsx
@@ -1,24 +1,59 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import {useGetRecordsQuery} from "../../app/api";
 import {RecordGrid, RecordGridProps} from "./RecordGrid";
 import {GridFilterModel, GridPaginationModel, GridSortModel} from "@mui/x-data-grid-premium";
 import dayjs from "dayjs";
+import {GridCallbackDetails} from "@mui/x-data-grid/models/api";
 
-export const AllRecordGrid = ({loading, ...props}: RecordGridProps) => {
-    // TODO: Take props
-    const [paginationModel, setPaginationModel] = React.useState<GridPaginationModel>(paginationModel ?? {
+export const AllRecordGrid = ({
+                                  loading,
+                                  filterModel: filterModelProps,
+                                  paginationModel: paginationModelProps,
+                                  sortModel: sortModelProps,
+                                  onFilterModelChange: onFilterModelChangeProps,
+                                  onPaginationModelChange: onPaginationModelChangeProps,
+                                  onSortModelChange: onSortModelChangeProps,
+                                  ...props
+                              }: RecordGridProps) => {
+    const [filterModelInternal, setFilterModelInternal] = React.useState<GridFilterModel>({
+        items: [],
+    })
+    const [paginationModelInternal, setPaginationModelInternal] = React.useState<GridPaginationModel>({
         page: 0,
         pageSize: 25,
     })
+    const [sortModelInternal, setSortModelInternal] = React.useState<GridSortModel>([])
 
-    const [sortModel, setSortModel] = React.useState<GridSortModel>([])
-    const [filterModel, setFilterModel] = React.useState<GridFilterModel>({
-        items: [],
-    })
+    // Use provided method, otherwise internal state handling
+    const onFilterModelChange = (model: GridFilterModel, details: GridCallbackDetails<'filter'>) => {
+        if (onFilterModelChangeProps)
+            onFilterModelChangeProps(model, details)
+        else
+            setFilterModelInternal(model)
+    }
+
+    const onPaginationModelChange = (model: GridPaginationModel, details: GridCallbackDetails) => {
+        if (onPaginationModelChangeProps)
+            onPaginationModelChangeProps(model, details)
+        else
+            setPaginationModelInternal(model)
+    }
+
+    const onSortModelChange = (model: GridSortModel, details: GridCallbackDetails) => {
+        if (onSortModelChangeProps)
+            onSortModelChangeProps(model, details)
+        else
+            setSortModelInternal(model)
+    }
+
+    const filterModel = useMemo(() => filterModelProps ?? filterModelInternal, [filterModelProps, filterModelInternal])
+    const paginationModel = useMemo(() => paginationModelProps ?? paginationModelInternal, [paginationModelProps, paginationModelInternal])
+    const sortModel = useMemo(() => sortModelProps ?? sortModelInternal, [sortModelProps, sortModelInternal])
+
     const queryOptions = React.useMemo(
         () => {
             return {
-                sortModel,
+                sortModel: sortModel,
                 filterModel: {
                     ...filterModel,
                     items: filterModel.items.map(item => ({
@@ -40,15 +75,15 @@ export const AllRecordGrid = ({loading, ...props}: RecordGridProps) => {
             loading={isLoading || loading}
             rowCount={data?.count ?? 0}
             pageSizeOptions={[10, 25, 50, 100]}
-            paginationModel={paginationModel}
-            sortModel={sortModel}
+            paginationModel={paginationModelInternal}
+            sortModel={sortModelInternal}
             filterModel={filterModel}
             paginationMode="server"
             sortingMode="server"
             filterMode="server"
-            onPaginationModelChange={setPaginationModel}
-            onSortModelChange={setSortModel}
-            onFilterModelChange={setFilterModel}
+            onPaginationModelChange={onPaginationModelChange}
+            onSortModelChange={onSortModelChange}
+            onFilterModelChange={onFilterModelChange}
             {...props}
         />
     )
