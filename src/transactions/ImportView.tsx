37230c5796a3334c482e874fd58a700c0ec58a46
Johannes Ruthmann <j.ruthmann@online.de>
2025-03-30 23:54:44 +0200

    #3: add upload file button

--- src/transactions/ImportView.tsx
+++ src/transactions/ImportView.tsx
@@ -1,34 +1,40 @@
 import {Box} from "@mui/material";
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import green from "@mui/material/colors/green";
 import {red} from '@mui/material/colors';
-import { useImportCsvMutation } from "../app/api";
+import {useImportCsvMutation} from "../app/api";
 
 export const ImportView = ({}) => {
     const [importFiles, {isSuccess, isError}] = useImportCsvMutation()
     const [dragging, setDragging] = useState(false)
+    const inputRef = useRef<HTMLInputElement>(null)
 
-    const handleDrop = (evt: React.DragEvent<HTMLDivElement>) => {
-        evt.preventDefault()
-        setDragging(false)
-
-        const droppedFiles = Array.from(evt.dataTransfer?.files ?? []);
+    const handleFiles = (files: File[]) => {
         const payload: string[] = []
 
-        droppedFiles.forEach((item) => {
+        files.forEach((item) => {
             const reader = new FileReader()
 
             reader.addEventListener("load", (evt) => {
                 payload.push(evt.target?.result as string)
 
-                if (payload.length === droppedFiles.length) {
+                if (payload.length === files.length) {
                     importFiles(payload)
                 }
             })
 
             reader.readAsDataURL(item)
         })
+    }
+
+    const handleDrop = (evt: React.DragEvent<HTMLDivElement>) => {
+        evt.preventDefault()
+        setDragging(false)
+
+        const droppedFiles = Array.from(evt.dataTransfer?.files ?? []);
+
+        handleFiles(droppedFiles)
     }
 
     const handleDragOver = (evt: React.DragEvent<HTMLDivElement>) => {
@@ -38,6 +44,16 @@ export const ImportView = ({}) => {
 
     const handleDragLeave = () => {
         setDragging(false)
+    }
+
+    const handleBoxClick = () => {
+        inputRef.current?.click()
+    }
+
+    const handleFileChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        if (evt.target.files !== null) {
+            handleFiles(Array.from(evt.target.files))
+        }
     }
 
     let bgcolor = "#fff"
@@ -67,15 +83,20 @@ export const ImportView = ({}) => {
             justifyContent: "center",
             alignItems: "center",
             transition: "all 220ms ease-in-out",
-            textAlign: "center"
+            textAlign: "center",
+            cursor: "pointer",
         }}
              onDragOver={handleDragOver}
              onDragLeave={handleDragLeave}
-             onDrop={handleDrop}>
+             onDrop={handleDrop}
+             onClick={handleBoxClick}
+        >
             <Box>
                 <FileDownloadIcon sx={{fontSize: "3rem"}}/>
                 <p>Upload CSV file</p>
             </Box>
+
+            <input type={"file"} ref={inputRef} style={{display: "none"}} onChange={handleFileChange}/>
         </Box>
     )
 }
