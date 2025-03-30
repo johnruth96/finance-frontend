50ad22002a48d304186a401fea0f2579e6e27c93
Johannes Ruthmann <j.ruthmann@online.de>
2025-03-30 11:36:13 +0200

    #2: add Rule interface

--- src/transactions/TransactionGridAction/CreateRecordAction.tsx
+++ src/transactions/TransactionGridAction/CreateRecordAction.tsx
@@ -1,26 +1,38 @@
 import {ButtonProps} from './types'
 import {GridActionsCellItem} from '@mui/x-data-grid-premium'
 import {AddCircle} from '@mui/icons-material'
-import {useCreateRecordMutation} from '../../app/api'
+import {useCreateRecordMutation, useGetCategoriesQuery, useGetContractsQuery} from '../../app/api'
 import React from 'react'
 import {red} from "@mui/material/colors";
 import {enqueueSnackbar} from "notistack";
 import {RecordFactory} from '../factory/factory';
+import {TRANSFORM_RULES} from "../../app/rules";
 
 export const CreateRecordAction = ({row}: ButtonProps) => {
-    const [createRecord, {isLoading, isError}] = useCreateRecordMutation()
+    const [createRecord, createStatus] = useCreateRecordMutation()
+    const {data: categories, ...getCategoriesStatus} = useGetCategoriesQuery()
+    const {data: contracts, ...getContractsStatus} = useGetContractsQuery()
 
     const handleClick = () => {
-        const factory = new RecordFactory()
-        const record = factory.createRecord(row)
+        if (categories && contracts) {
+            const factory = new RecordFactory()
+            factory.setRules(TRANSFORM_RULES)
+            factory.setCategories(categories)
+            factory.setContracts(contracts)
 
-        console.debug("Creating record:", record)
+            const record = factory.createRecord(row)
 
-        createRecord(record).unwrap().catch((err: any) => {
-            enqueueSnackbar(JSON.stringify(err), {variant: "error"})
-            console.error(err)
-        })
+            console.debug("Creating record:", record)
+
+            createRecord(record).unwrap().catch((err: any) => {
+                enqueueSnackbar(JSON.stringify(err), {variant: "error"})
+                console.error(err)
+            })
+        }
     }
+
+    const isError = createStatus.isError || getCategoriesStatus.isError || getContractsStatus.isError
+    const isLoading = createStatus.isLoading || getCategoriesStatus.isLoading || getContractsStatus.isLoading
 
     return (
         <GridActionsCellItem
--- src/transactions/factory/factory.ts
+++ src/transactions/factory/factory.ts
@@ -1,36 +1,108 @@
-/*
- DO NOT COMMIT
- */
 
 import {Category, Contract, RecordType} from "../../app/types";
 import dayjs from "dayjs";
 import {Transaction} from "../types";
 
-// TODO: #2 Add Rule Interface and serialization
+export interface RecordDraft extends Partial<Omit<RecordType, 'category' | 'contract'>> {
+    category?: string | number | null
+    contract?: string | number | null
+}
+
+export interface EqualsCondition {
+    field: keyof Transaction
+    operator: "equals"
+    value: string | number | boolean
+}
+
+export interface StartsWithCondition {
+    field: keyof Transaction
+    operator: "startsWith"
+    value: string
+}
+
+export type Condition = EqualsCondition | StartsWithCondition
+
+export interface TransformRule {
+    conditions: Condition[]
+    action: RecordDraft | ((transaction: Transaction) => RecordDraft)
+}
+
+const matchTransaction = (transaction: Transaction, rule: TransformRule) => {
+    return rule.conditions.every(cond => {
+        if (cond.operator === "equals") {
+            return transaction[cond.field] === cond.value
+        } else if (cond.operator === "startsWith") {
+            const value = transaction[cond.field]
+            return typeof value === "string" ? value.startsWith(cond.value) : false
+        }
+    })
+}
+
+const applyRule = (rule: TransformRule, transaction: Transaction): RecordDraft => {
+    if (typeof rule.action === "function") {
+        return rule.action(transaction)
+    } else {
+        return rule.action
+    }
+}
+
 export class RecordFactory {
     private categories: Category[] = [];
     private contracts: Contract[] = [];
+    private rules: TransformRule[] = [];
 
-    // private rules: TransformRule[] = [];
-
-    createRecord(transaction: Transaction): Omit<RecordType, "id"> {
+    prepareRecord(transaction: Transaction): RecordDraft {
         const subjectCandidates = [transaction.creditor, transaction.purpose, transaction.transaction_type, "<empty>"]
+        const subject = subjectCandidates.find(s => s !== "") as string
+        const date = dayjs(transaction.value_date).format("YYYY-MM-DD")
 
         return {
-            subject: subjectCandidates.find(s => s !== "") as string,
-            date: dayjs(transaction.value_date).format("YYYY-MM-DD"),
+            subject: subject,
+            date: date,
             amount: transaction.amount,
             account: 1, // FIXME
             transactions: [transaction.id],
-            category: 50,
+            category: null,
             contract: null,
             counter_booking: null,
         }
     }
 
-    /* setRules(rules: TransformRule[]) {
+    createRecord(transaction: Transaction): Omit<RecordType, "id"> {
+        const record = this.prepareRecord(transaction)
+
+        for (const rule of this.rules) {
+            if (matchTransaction(transaction, rule)) {
+                const draft = applyRule(rule, transaction)
+
+                for (const [attribute, value] of Object.entries(draft)) {
+                    if (attribute === "category") {
+                        if (typeof value === "string") {
+                            record.category = this.getCategoryId(value)
+                        } else {
+                            record.category = value
+                        }
+
+                    } else if (attribute === "contract") {
+                        if (typeof value === "string") {
+                            record.contract = this.getContractId(value)
+                        } else {
+                            record.contract = value
+                        }
+                    } else {
+                        // @ts-ignore
+                        record[attribute] = value
+                    }
+                }
+            }
+        }
+
+        return record as RecordType
+    }
+
+    setRules(rules: TransformRule[]) {
         this.rules = rules
-    }*/
+    }
 
     setCategories(categories: Category[]) {
         this.categories = categories
